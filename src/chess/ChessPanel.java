bdc2a7dd3938a3ffb650883b8d38fd0956b1ca85
ncevans13 <ncevans13@gmail.com>
2014-03-16 16:09:49 -0400

    fev

--- src/chess/ChessPanel.java
+++ src/chess/ChessPanel.java
@@ -8,71 +8,82 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-	public class ChessPanel extends JPanel { 
+public class ChessPanel extends JPanel { 
 
-		private JButton[][] board; 
-		private ChessModel model;
-		private final int BDSIZE = 8;
+	private JButton[][] board; 
+	private ChessModel model;
+	private final int BDSIZE = 8;
+	private int sRow = -1;
+	private int sCol = -1;
 
-		// declare other instance variables as needed 
+	// declare other instance variables as needed 
 
-		private ButtonListener buttonListener = new ButtonListener(); 
+	private ButtonListener buttonListener = new ButtonListener(); 
 
-		public ChessPanel() { 
-			board = new JButton[BDSIZE][BDSIZE];
-			model = new ChessModel();
-			setLayout(new GridLayout(8,8));
-			for (int row = 0; row < BDSIZE; row++) {
-				for (int col = 0; col < BDSIZE; col++) {
-					board[row][col] = new JButton("");
-					board[row][col].setFont(new Font("Dialog",1,36));
-					add(board[row][col]);
-				}
+	public ChessPanel() { 
+		board = new JButton[BDSIZE][BDSIZE];
+		model = new ChessModel();
+		setLayout(new GridLayout(8,8));
+		for (int row = 0; row < BDSIZE; row++) {
+			for (int col = 0; col < BDSIZE; col++) {
+				board[row][col] = new JButton("");
+				board[row][col].setFont(new Font("Dialog",1,36));
+				add(board[row][col]);
+				board[row][col].addActionListener(buttonListener);
 			}
-			displayBoard();
-			
 		}
+		displayBoard();
 
-		// method that updates the board 
-		private void displayBoard() { 
-			for (int row = 0; row < BDSIZE; row++) {
-				for (int col = 0; col < BDSIZE; col++) {
-					board[row][col].setOpaque(true);
-					board[row][col].setBorderPainted(false);
-					if (row % 2 == 0){
-						if (col % 2 == 0){
-							board[row][col].setBackground(Color.GRAY);
-						}
-						else{
-							board[row][col].setBackground(Color.WHITE);
-						}
+	}
+
+	// method that updates the board 
+	private void displayBoard() { 
+		for (int row = 0; row < BDSIZE; row++) {
+			for (int col = 0; col < BDSIZE; col++) {
+				board[row][col].setOpaque(true);
+				board[row][col].setBorderPainted(false);
+				if (row % 2 == 0){
+					if (col % 2 == 0){
+						board[row][col].setBackground(Color.WHITE);
 					}
 					else{
-						if (col % 2 == 0){
-							board[row][col].setBackground(Color.WHITE);
-						}
-						else{
-							board[row][col].setBackground(Color.GRAY);
-						}
+						board[row][col].setBackground(Color.GRAY);
 					}
-					try{
-						board[row][col].setText(model.pieceAt(row,col).icon());
+				}
+				else{
+					if (col % 2 == 0){
+						board[row][col].setBackground(Color.GRAY);
 					}
-					catch(NullPointerException e){
-						
+					else{
+						board[row][col].setBackground(Color.WHITE);
 					}
-					
+				}
+				try{
+					board[row][col].setText(model.pieceAt(row,col).icon());
+				}
+				catch(NullPointerException e){
+					board[row][col].setText("");
+				}
+			}
+		}
+	} 
+
+	// add other helper methods as needed 
+
+	// inner class that represents action listener for buttons 
+	private class ButtonListener implements ActionListener { 
+
+		public void actionPerformed(ActionEvent event) { 
+
+			for (int row = 0; row < BDSIZE; row++) {
+				for (int col = 0; col < BDSIZE; col++) {
+					if (board[row][col] == event.getSource()) {
+						sRow = row;
+						sCol = col;
+						board[row][col].setBackground(Color.BLUE);
+					}
 				}
 			}
 		} 
-
-		// add other helper methods as needed 
-
-		// inner class that represents action listener for buttons 
-		private class ButtonListener implements ActionListener { 
-
-			public void actionPerformed(ActionEvent event) { 
-				// complete this 
-			} 
-		} 
 	} 
+} 
