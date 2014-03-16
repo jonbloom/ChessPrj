913225d34ea5f4d187178136bab6d225711a34d7
Jon Bloom <github@jon.tw>
2014-03-16 18:43:54 -0400

    hiul

--- src/chess/ChessModel.java
+++ src/chess/ChessModel.java
@@ -7,110 +7,121 @@ public class ChessModel implements IChessModel {
 	private ChessPiece[][] board;
 	private Player player;
 	private final static int BDSIZE = 8;
-	
-	
+
+
 	public ChessModel() {
 		board = new ChessPiece[BDSIZE][BDSIZE];
 		// Set up specific pieces, to start board.
-		
+
 		//Pawns
 		for (int i = 0; i < 8; i++) {
 			board[1][i] = new Pawn(Player.BLACK);
 			board[6][i] = new Pawn(Player.WHITE);
 		}
-		
+
 		//Rook
 		board[0][0] = new Rook(Player.BLACK);
 		board[0][7] = new Rook(Player.BLACK);
 		board[7][0] = new Rook(Player.WHITE);
 		board[7][7] = new Rook(Player.WHITE);
-		
+
 		//Knight
 		board[0][1] = new Knight(Player.BLACK);
 		board[0][6] = new Knight(Player.BLACK);
 		board[7][1] = new Knight(Player.WHITE);
 		board[7][6] = new Knight(Player.WHITE);
-		
+
 		//Bishop
 		board[0][2] = new Bishop(Player.BLACK);
 		board[0][5] = new Bishop(Player.BLACK);
 		board[7][2] = new Bishop(Player.WHITE);
 		board[7][5] = new Bishop(Player.WHITE);
-		
+
 		//Queen
 		board[0][3] = new Queen(Player.BLACK);
 		board[7][4] = new Queen(Player.WHITE);
-		
+
 		//King 
 		board[0][4] = new King(Player.BLACK);
 		board[7][3] = new King(Player.WHITE);
 	}
-	
+
 	public boolean isComplete() {
-		
+		return true;
 	}
 
 	public boolean isValidMove(Move move) {
-		if (board[move.fromRow][move.fromColumn].isValidMove(move, board)) {
-			return true;
-		}
-		return false;
+		return (board[move.fromRow][move.fromColumn].isValidMove(move, board));
 	}
 
 	public void move(Move move) {
-		move.fromRow = move.toRow;
-		move.fromColumn = move.toColumn;
+		board[move.toRow][move.toColumn] = board[move.fromRow][move.fromColumn];
+		board[move.fromRow][move.fromColumn] = null;
 	}
 
 	public boolean inCheck(Player player) {
 		if (player == Player.WHITE){
-<<<<<<< HEAD
-			return true;
-		}
-		else{
-			return false;
-		}
-		
-=======
 			int kingR = -1,kingC = -1;
 			for (int row = 0; row < BDSIZE; row++)
-				for (int col = 0; col < BDSIZE; col++)
-					if (pieceAt(row,col).type().equals("King") && pieceAt(row,col).player() == Player.WHITE){
-						kingR = row;
-						kingC = col;
+				for (int col = 0; col < BDSIZE; col++){
+					try{
+						if (pieceAt(row,col).type().equals("King") && pieceAt(row,col).player() == Player.WHITE){
+							kingR = row;
+							kingC = col;
+						}
 					}
+					catch(NullPointerException e){
+					}
+				}
+
 			for (int row = 0; row < BDSIZE; row++)
-				for (int col = 0; col < BDSIZE; col++)
-					if (pieceAt(row,col).player() == Player.BLACK && pieceAt(row,col).isValidMove(new Move(row,col,kingR,kingC), board)){
-						return true;
+				for (int col = 0; col < BDSIZE; col++){
+					try{
+						if (pieceAt(row,col).player() == Player.BLACK && pieceAt(row,col).isValidMove(new Move(row,col,kingR,kingC), board)){
+							return true;
+						}
 					}
+					catch(NullPointerException e){
+					}
+				}
 		}
 		else{
 			int kingR = -1,kingC = -1;
 			for (int row = 0; row < BDSIZE; row++)
-				for (int col = 0; col < BDSIZE; col++)
-					if (pieceAt(row,col).type().equals("King") && pieceAt(row,col).player() == Player.BLACK){
-						kingR = row;
-						kingC = col;
+				for (int col = 0; col < BDSIZE; col++){
+
+					try{
+						if (pieceAt(row,col).type().equals("King") && pieceAt(row,col).player() == Player.BLACK){
+							kingR = row;
+							kingC = col;
+						}
 					}
+					catch(NullPointerException e){
+					}
+				}
+
 			for (int row = 0; row < BDSIZE; row++)
-				for (int col = 0; col < BDSIZE; col++)
-					if (pieceAt(row,col).player() == Player.WHITE && pieceAt(row,col).isValidMove(new Move(row,col,kingR,kingC), board)){
-						return true;
+				for (int col = 0; col < BDSIZE; col++){
+					try{
+						if (pieceAt(row,col).player() == Player.WHITE && pieceAt(row,col).isValidMove(new Move(row,col,kingR,kingC), board)){
+							return true;
+						}
 					}
+					catch(NullPointerException e){
+					}
+				}
 		}
 		return false;
->>>>>>> FETCH_HEAD
 	}
 
 	public Player currentPlayer() {
 		return player;
 	}
-	
+
 	public int numRows() {
 		return BDSIZE;
 	}
-	
+
 	public int numColumns() {
 		return BDSIZE;
 	}
--- src/chess/ChessPanel.java
+++ src/chess/ChessPanel.java
@@ -8,95 +8,90 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class ChessPanel extends JPanel { 
+	public class ChessPanel extends JPanel { 
 
-<<<<<<< HEAD
-	private JButton[][] board; 
-	private ChessModel model;
-	private final int BDSIZE = 8;
-	private int sRow = -1;
-	private int sCol = -1;
-=======
 		private JButton[][] board; 
 		private ChessModel model;
 		private final int BDSIZE = 8;
 		private int sRow = -1;
 		private int sCol = 01;
->>>>>>> FETCH_HEAD
 
-	// declare other instance variables as needed 
+		// declare other instance variables as needed 
 
-	private ButtonListener buttonListener = new ButtonListener(); 
+		private ButtonListener buttonListener = new ButtonListener(); 
 
-	public ChessPanel() { 
-		board = new JButton[BDSIZE][BDSIZE];
-		model = new ChessModel();
-		setLayout(new GridLayout(8,8));
-		for (int row = 0; row < BDSIZE; row++) {
-			for (int col = 0; col < BDSIZE; col++) {
-				board[row][col] = new JButton("");
-				board[row][col].setFont(new Font("Dialog",1,36));
-				add(board[row][col]);
-				board[row][col].addActionListener(buttonListener);
-			}
-		}
-		displayBoard();
-
-	}
-
-	// method that updates the board 
-	private void displayBoard() { 
-		for (int row = 0; row < BDSIZE; row++) {
-			for (int col = 0; col < BDSIZE; col++) {
-				board[row][col].setOpaque(true);
-				board[row][col].setBorderPainted(false);
-				if (row % 2 == 0){
-					if (col % 2 == 0){
-						board[row][col].setBackground(Color.WHITE);
-					}
-					else{
-						board[row][col].setBackground(Color.GRAY);
-					}
-				}
-				else{
-					if (col % 2 == 0){
-						board[row][col].setBackground(Color.GRAY);
-					}
-<<<<<<< HEAD
-					else{
-						board[row][col].setBackground(Color.WHITE);
-=======
-					catch(NullPointerException e){
-						board[row][col].setText("");
->>>>>>> FETCH_HEAD
-					}
-				}
-				try{
-					board[row][col].setText(model.pieceAt(row,col).icon());
-				}
-				catch(NullPointerException e){
-					board[row][col].setText("");
-				}
-			}
-		}
-	} 
-
-	// add other helper methods as needed 
-
-	// inner class that represents action listener for buttons 
-	private class ButtonListener implements ActionListener { 
-
-		public void actionPerformed(ActionEvent event) { 
-
+		public ChessPanel() { 
+			board = new JButton[BDSIZE][BDSIZE];
+			model = new ChessModel();
+			setLayout(new GridLayout(8,8));
 			for (int row = 0; row < BDSIZE; row++) {
 				for (int col = 0; col < BDSIZE; col++) {
-					if (board[row][col] == event.getSource()) {
-						sRow = row;
-						sCol = col;
-						board[row][col].setBackground(Color.BLUE);
+					board[row][col] = new JButton("");
+					board[row][col].setFont(new Font("Dialog",1,36));
+					add(board[row][col]);
+				}
+			}
+			displayBoard();
+			
+		}
+
+		// method that updates the board 
+		private void displayBoard() { 
+			for (int row = 0; row < BDSIZE; row++) {
+				for (int col = 0; col < BDSIZE; col++) {
+					board[row][col].setOpaque(true);
+					board[row][col].setBorderPainted(false);
+					if (row % 2 == 0){
+						if (col % 2 == 0){
+							board[row][col].setBackground(Color.GRAY);
+						}
+						else{
+							board[row][col].setBackground(Color.WHITE);
+						}
+					}
+					else{
+						if (col % 2 == 0){
+							board[row][col].setBackground(Color.WHITE);
+						}
+						else{
+							board[row][col].setBackground(Color.GRAY);
+						}
+					}
+					try{
+						board[row][col].setText(model.pieceAt(row,col).icon());
+					}
+					catch(NullPointerException e){
+						board[row][col].setText("");
+					}
+					
+				}
+			}
+		} 
+
+		// add other helper methods as needed 
+
+		// inner class that represents action listener for buttons 
+		private class ButtonListener implements ActionListener { 
+
+			public void actionPerformed(ActionEvent event) { 
+
+				for (int row = 0; row < BDSIZE; row++) {
+					for (int col = 0; col < BDSIZE; col++) {
+						
+						if (sRow > -1 && sCol > -1) {
+							if (board[row][col] == event.getSource()) {
+								board[row][col]
+							}
+						}
+						
+						if (board[row][col] == event.getSource()) {
+							sRow = row;
+							sCol = col;
+							board[sRow][sCol].setBackground(Color.BLUE);
+							
+						}
 					}
 				}
 			}
 		} 
 	} 
-} 
